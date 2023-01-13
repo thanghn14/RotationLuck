3de46b011d7cc75d633f2909c70cf5d4f1fef6da
thanghn14 <thanghongocql@gmail.com>
2023-01-13 14:31:01 +0700

    Changes to be committed: 	new file:   src/assets/mp3/xo-so.mp3 	modified:   src/components/Random.jsx

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -3,7 +3,7 @@ import "../assets/css/random.css";
 import FireWork from "./FireWork";
 import { dummyData } from "./_mock";
 import musicSuccess from "../assets/mp3/congrats.mp3";
-import musicPlay from "../assets/mp3/musicPlay.mp3";
+import musicPlay from "../assets/mp3/xo-so.mp3";
 import Audio from "./Audio";
 
 const Random = () => {
@@ -200,7 +200,7 @@ const Random = () => {
         <thead>
           <tr className="thead">
             <th>Giải thưởng</th>
-            <th>Mã nhân viên</th>
+            <th>Mã số</th>
           </tr>
         </thead>
         <tbody>
